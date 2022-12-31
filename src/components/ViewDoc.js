7eb0a34d675c7db75fbd72a5d461a4e52a3d88f8
Henry Ofoe Agbi-Kaiser <51025058+Kaiser-h@users.noreply.github.com>
2022-12-31 12:49:25 +0000

    main

--- src/components/ViewDoc.js
+++ src/components/ViewDoc.js
@@ -1,36 +1,32 @@
-import React, {  } from "react";
+import React, { useState } from "react";
 import "./ViewDoc.css";
 import { useLocation } from "react-router-dom";
 import Container from "react-bootstrap/esm/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/esm/Button";
 import { useNavigate } from "react-router-dom";
-// import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+
+import { Document, Page } from "react-pdf";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 
 
-// import path from 'path';
-// import fs from 'fs';
-
-// import { pdfjs } from 'react-pdf';
-// pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
 
 
-// const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
-// const pdfWorkerPath = path.join(pdfjsDistPath, 'build', 'pdf.worker.js');
-// fs.copyFileSync(pdfWorkerPath, './dist/pdf.worker.js');
+
 
 
 
 
 function ViewDoc(props) {
   const location = useLocation();
-  // const [numPages, setNumPages] = useState(null);
+  const [numPages, setNumPages] = useState(null);
 
   const navigate = useNavigate();
 
-  // function onDocumentLoadSuccess({ numPages }) {
-  //   setNumPages(numPages);
-  // }
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
   return (
     <div className="viewdoc">
@@ -42,8 +38,8 @@ function ViewDoc(props) {
         </Container>
       </Navbar>
       <Container className="co1">
-        <div><h1>{location.state.pdfurl}</h1></div>
-        {/* <Document
+        <p>{location.state.pdfurl}</p>
+        <Document
           file={location.state.pdfurl}
           onLoadSuccess={onDocumentLoadSuccess}
         >
@@ -54,14 +50,14 @@ function ViewDoc(props) {
               width="800"
             />
           ))}
-        </Document> */}
-      </Container>
+        </Document>
       {/* <iframe
         src={location.state.pdfurl}
         title={location.state.pdfurl}
         width="700"
         className="myiframe"
       /> */}
+      </Container>
     </div>
   );
 }
